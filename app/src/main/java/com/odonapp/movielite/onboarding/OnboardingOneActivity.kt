335106fd946b6bd30bc5583864dffadba292e196
unknown <dhonadwi2@gmail.com>
2022-03-22 14:22:18 +0700

    start activity

--- app/src/main/java/com/odonapp/movielite/onboarding/OnboardingOneActivity.kt
+++ app/src/main/java/com/odonapp/movielite/onboarding/OnboardingOneActivity.kt
@@ -1,13 +1,32 @@
 package com.odonapp.movielite.onboarding
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odonapp.movielite.R
+import com.odonapp.movielite.sign.SignInActivity
+import com.odonapp.movielite.sign.SignUpActivity
+import kotlinx.android.synthetic.main.activity_onboarding_one.*
 
 class OnboardingOneActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_one)
+
+        btn_home.setOnClickListener {
+            var intent = Intent(this@OnboardingOneActivity, OnboardingTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_daftar.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                finishAffinity()
+            }
+
+            var intent = Intent(this@OnboardingOneActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/odonapp/movielite/onboarding/OnboardingThreeActivity.kt
+++ app/src/main/java/com/odonapp/movielite/onboarding/OnboardingThreeActivity.kt
@@ -1,13 +1,23 @@
 package com.odonapp.movielite.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.odonapp.movielite.R
+import com.odonapp.movielite.sign.SignInActivity
+import kotlinx.android.synthetic.main.activity_onboarding_three.*
 
 class OnboardingThreeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding_three)
+
+        btn_home.setOnClickListener {
+            finishAffinity()
+            var intent = Intent(this@OnboardingThreeActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }

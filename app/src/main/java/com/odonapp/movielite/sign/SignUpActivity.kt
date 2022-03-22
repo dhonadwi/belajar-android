440fcc0d43df4eec368522d9e6665aaeaa0f1aa2
unknown <dhonadwi2@gmail.com>
2022-03-22 11:56:25 +0700

    fixing home.xml

--- app/src/main/java/com/odonapp/movielite/sign/SignUpActivity.kt
+++ app/src/main/java/com/odonapp/movielite/sign/SignUpActivity.kt
@@ -1,12 +1,13 @@
-package com.odonapp.movielite
+package com.odonapp.movielite.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odonapp.movielite.R
 
-class SignInActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_sign_up)
     }
 }
--- app/src/main/java/com/odonapp/movielite/sign/SignUpPhotoscreenActivity.kt
+++ app/src/main/java/com/odonapp/movielite/sign/SignUpPhotoscreenActivity.kt
@@ -1,7 +1,8 @@
-package com.odonapp.movielite
+package com.odonapp.movielite.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odonapp.movielite.R
 
 class SignUpPhotoscreenActivity : AppCompatActivity() {
 

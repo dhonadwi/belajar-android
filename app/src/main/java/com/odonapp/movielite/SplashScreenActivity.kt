32051e20c8942686a9312d355b0ae932843ec188
unknown <dhonadwi2@gmail.com>
2022-03-22 13:57:21 +0700

    create splashscreen

--- app/src/main/java/com/odonapp/movielite/SplashScreenActivity.kt
+++ app/src/main/java/com/odonapp/movielite/SplashScreenActivity.kt
@@ -1,12 +1,22 @@
 package com.odonapp.movielite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.odonapp.movielite.onboarding.OnboardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, OnboardingOneActivity::class.java)
+            startActivity(intent)
+            finish()
+        },5000)
     }
 }
